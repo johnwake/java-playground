be296bd20e5e4ae80f51afe7ba2054e80b122a61
John Wakeling <john.wakeling@fundingcircle.com>
2016-04-05 14:18:28 +0100

    first push of FizzBuzz in Java

---
+++ FizzBuzz2/src/FizzBuzz.java
@@ -0,0 +1,14 @@
+/**
+ * Created by jwakeling on 05/04/2016.
+ */
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100; i++) {
+            if (((i % 3) == 0) && ((i % 5) == 0)) System.out.println("FizzBuzz");
+            else if ((i % 3) == 0) System.out.println("Fizz");
+            else if ((i % 5) == 0) System.out.println("Buzz");
+            else System.out.println(i);
+        }
+    }
+}
